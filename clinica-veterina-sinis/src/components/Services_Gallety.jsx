97d951f2a80247755862cc14b942db2ea8c45c5b
Milton Sojo <yousif.sojo@ucr.ac.cr>
2024-09-10 12:52:07 -0600

    Nuestros Servicios Version imagen fija

--- clinica-veterina-sinis/src/components/Services_Gallety.jsx
+++ clinica-veterina-sinis/src/components/Services_Gallety.jsx
@@ -1,14 +1,16 @@
-function Services_Gallety({ title, subtitle, text, imageSrc, className }) {
+function Services_Gallery({ title, subtitle, text, imageSrc, className }) {
   return (
     <div className={`lg:w-1/3 sm:w-1/2 p-4 ${className}`}>
-      <div className="flex relative h-64">
-        <img
-          alt="gallery"
-          className="absolute inset-0 w-full h-full object-cover object-center"
-          src={imageSrc}
-          style={{ objectFit: "cover" }} // Asegurar que la imagen cubra todo el contenedor
-        />
-        <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+      <div className="flex flex-col h-full">
+        <div className="relative h-64">
+          <img
+            alt="gallery"
+            className="absolute inset-0 w-full h-full object-cover object-center"
+            src={imageSrc}
+            style={{ objectFit: "cover" }} // Asegurar que la imagen cubra todo el contenedor
+          />
+        </div>
+        <div className="px-8 py-4 bg-white border-t-4 border-gray-200 flex-grow">
           <h2 className="tracking-widest text-2xl title-font font-medium text-indigo-500 mb-1">
             {title}
           </h2>
@@ -22,4 +24,4 @@ function Services_Gallety({ title, subtitle, text, imageSrc, className }) {
   );
 }
 
-export default Services_Gallety;
+export default Services_Gallery;
