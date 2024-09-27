a367bae6c7db9f680118d4d67691721d08588f26
andrey1099-ucr <lester.pereira@ucr.ac.cr>
2024-09-27 12:55:33 -0600

    FEAT: Agregar página para consultar clientes

---
+++ frontend/src/routes/ConsultClients.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function ConsultClients() {
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    );
+}
+
+export default ConsultClients;
