7d46a49ad83418785c71a749be768f49a698bdec
Milton Sojo <yousif.sojo@ucr.ac.cr>
2024-10-08 12:13:08 -0600

    FIX: Se añaden los precios y el tiempo de tratamiento en servicios

--- frontend/src/components/Services_Gallery.jsx
+++ frontend/src/components/Services_Gallery.jsx
@@ -1,25 +1,51 @@
-function Services_Gallery({ title, subtitle, text, imageSrc, className }) {
+import { Card, CardContent, Typography, Grow } from "@mui/material";
+
+function Services_Gallery({
+  title,
+  subtitle,
+  text,
+  imageSrc,
+  className,
+  price,
+  duration,
+}) {
   return (
     <div className={`lg:w-1/3 sm:w-1/2 p-4 ${className}`}>
-      <div className="flex flex-col h-full">
-        <div className="relative h-64">
-          <img
-            alt="gallery"
-            className="absolute inset-0 w-full h-full object-cover object-center"
-            src={imageSrc}
-            style={{ objectFit: "cover" }}
-          />
-        </div>
-        <div className="px-8 py-4 bg-bgsecondary border-t-4 border-gray-200 flex-grow">
-          <h2 className="tracking-widest text-2xl title-font font-semibold text-secondary mb-1 font-montserrat">
-            {title}
-          </h2>
-          <h1 className="title-font text-lg font-medium text-elemsec mb-3 font-montserrat">
-            {subtitle}
-          </h1>
-          <p className="leading-relaxed font-montserrat">{text}</p>
-        </div>
-      </div>
+      <Grow in={true} timeout={1000}>
+        <Card className="flex flex-col h-full">
+          <div className="relative h-64">
+            <img
+              alt="gallery"
+              className="absolute inset-0 w-full h-full object-cover object-center"
+              src={imageSrc}
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+          <div className="px-8 py-4 bg-bgsecondary border-t-4 border-gray-200 flex-grow">
+            <div className="flex justify-between items-center">
+              <h2 className="tracking-widest text-2xl title-font font-semibold text-secondary mb-1 font-montserrat">
+                {title}
+              </h2>
+              <Typography
+                variant="body2"
+                color="green"
+                className="font-montserrat">
+                {price}
+              </Typography>
+            </div>
+            <h1 className="title-font text-lg font-medium text-elemsec mb-3 font-montserrat">
+              {subtitle}
+            </h1>
+            <p className="leading-relaxed font-montserrat">{text}</p>
+            <Typography
+              variant="overline"
+              color="textSecondary"
+              className="font-montserrat mt-2">
+              {duration}
+            </Typography>
+          </div>
+        </Card>
+      </Grow>
     </div>
   );
 }
--- frontend/src/routes/Services.jsx
+++ frontend/src/routes/Services.jsx
@@ -25,70 +25,77 @@ function Services() {
               </p>
             </div>
             <div className="flex flex-wrap -m-8">
-              {/* Cada uno de estos es un cuadrito más */}
               <Services_Gallery
-                // ClassName es por si se le desea agregar otro atributo de clase aparte del default
                 className=""
                 title="Medicina Interna"
-                subtitle=""
                 text="Ofrecemos atención integral para enfermedades internas en mascotas, incluyendo diagnóstico y tratamiento de condiciones como diabetes, enfermedades respiratorias y trastornos gastrointestinales."
                 imageSrc="./src/assets/img/Services_medicina_interna.jpg"
+                price="Costo: Variable"
+                duration="Tiempo: 3 a 5 sesiones de 1 hora cada una"
               />
               <Services_Gallery
                 className=""
                 title="Laboratorio"
-                subtitle=""
                 text="Contamos con un laboratorio completamente equipado para realizar análisis clínicos, pruebas de sangre y exámenes de orina, lo que nos permite obtener resultados rápidos y precisos para un diagnóstico efectivo."
                 imageSrc="./src/assets/img/Services_laboratorio.jpg"
+                price="Costo: 15.000 CRC"
+                duration="Tiempo: 1 hora"
               />
               <Services_Gallery
                 className=""
                 title="Rayos X"
-                subtitle=""
                 text="Nuestra tecnología de rayos X de última generación permite obtener imágenes detalladas del interior de su mascota, facilitando la detección de fracturas, tumores y otras anomalías."
                 imageSrc="./src/assets/img/Services_RayosX.jpg"
+                price="Costo: 25.000 CRC"
+                duration="Tiempo: 45 minutos"
               />
               <Services_Gallery
                 className=""
                 title="Ultrasonido"
-                subtitle=""
                 text="Realizamos estudios de ultrasonido para evaluar órganos internos y tejidos blandos, siendo una herramienta clave para diagnósticos en tiempo real sin necesidad de cirugía."
                 imageSrc="./src/assets/img/Services_Ultrasonido.jpg"
+                price="Costo: 30.000 CRC"
+                duration="Tiempo: 30 a 60 minutos"
               />
               <Services_Gallery
                 className=""
                 title="Cirugía"
-                subtitle=""
                 text="Ofrecemos una amplia gama de procedimientos quirúrgicos, desde operaciones de rutina hasta cirugías más complejas, siempre con el máximo cuidado y atención a la recuperación de su mascota."
                 imageSrc="./src/assets/img/Services_Cirugia.jpg"
+                price="Costo: Variable"
+                duration="Tiempo: Dependiendo de la complejidad"
               />
               <Services_Gallery
                 className=""
                 title="Limpieza Dental"
-                subtitle=""
                 text="Brindamos servicios de limpieza dental profesional para mantener la salud bucal de su mascota, previniendo enfermedades periodontales y mejorando su bienestar general."
                 imageSrc="./src/assets/img/Services_Limpieza_Dental.jpg"
+                price="Costo: 18.000 CRC"
+                duration="Tiempo: 1 hora"
               />
               <Services_Gallery
                 className=""
                 title="Internamiento"
-                subtitle=""
                 text="Contamos con instalaciones adecuadas para el internamiento de mascotas que requieren atención constante, garantizando un ambiente seguro y cómodo durante su recuperación."
                 imageSrc="./src/assets/img/Services_Internamiento.jpg"
+                price="Costo: 30.000 CRC por día"
+                duration="Tiempo: Variable"
               />
               <Services_Gallery
                 className=""
                 title="Ortopedia"
-                subtitle=""
                 text="Ofrecemos atención especializada en ortopedia para tratar lesiones y enfermedades del sistema musculoesquelético, asegurando una recuperación efectiva y el bienestar de su mascota."
                 imageSrc="./src/assets/img/Services_Ortopedia.jpg"
+                price="Costo: 40.000 CRC"
+                duration="Tiempo: Dependiendo del tratamiento"
               />
               <Services_Gallery
                 className=""
                 title="Grooming"
-                subtitle=""
                 text="Nuestro servicio de grooming incluye baños, cortes de pelo y cuidados estéticos, ayudando a mantener la higiene y apariencia de su mascota, además de contribuir a su salud general."
                 imageSrc="./src/assets/img/Services_Grooming.jpg"
+                price="Costo: 12.000 CRC"
+                duration="Tiempo: 1.5 a 2 horas"
               />
             </div>
           </div>
