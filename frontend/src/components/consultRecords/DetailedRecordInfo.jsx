4e7937fddc3518e68e29c01bc2b7ec04be53f8b1
Andres-QG <andres.quesadagonzalez@ucr.ac.cr>
2024-10-27 11:04:50 -0600

    STYLE: DetailsRecordInfo estilo mejorado

--- frontend/src/components/consultRecords/DetailedRecordInfo.jsx
+++ frontend/src/components/consultRecords/DetailedRecordInfo.jsx
@@ -1,7 +1,24 @@
+import PropTypes from "prop-types";
 import { Modal, Box, IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import MonitorWeightIcon from "@mui/icons-material/MonitorWeight";
+import TreatmentIcon from "@mui/icons-material/MedicationLiquid";
+import VaccinesIcon from "@mui/icons-material/Vaccines";
+import HealingIcon from "@mui/icons-material/Healing";
 
 const DetailedRecordInfo = ({ open, handleClose, selectedItem }) => {
+  const details = {
+    consulta_id: selectedItem.consulta_id,
+    nombre_mascota: selectedItem.nombre_mascota,
+    fecha: selectedItem.fecha,
+    peso: selectedItem.peso,
+    sintomas: selectedItem.sintomas.split(","),
+    vacunas: selectedItem.vacunas.split(","),
+    tratamientos: selectedItem.tratamientos.split(","),
+  };
+
   return (
     <Modal open={open} onClose={handleClose}>
       <Box
@@ -26,33 +43,51 @@ const DetailedRecordInfo = ({ open, handleClose, selectedItem }) => {
         >
           <CloseIcon />
         </IconButton>
-        <h2 className="text-2xl font-bold mb-4 pt-4">
-          Detalles sobre el campo de expediente ID({selectedItem.consulta_id})
+        <h2 className="text-xl font-medium text-gray-700 mb-3 flex items-center">
+          <AssignmentIcon sx={{ mr: 1 }} />
+          Expediente de consulta ID: {details.consulta_id} de{" "}
+          {details.nombre_mascota}
         </h2>
-        <div className="mb-8">
-          <h3 className="text-xl font-semibold">Peso en Kilogramos:</h3>
-          <p className="text-gray-700">{selectedItem.peso}</p>
+        <div className="mb-3 flex items-center text-gray-500 text-sm">
+          <CalendarTodayIcon sx={{ mr: 1 }} />
+          Fecha: {details.fecha}
         </div>
         <div className="mb-8">
-          <h3 className="text-xl font-semibold">Síntomas presentados</h3>
-          <ul className="list-disc list-inside text-gray-700">
-            {selectedItem.sintomas.split(",").map((sintoma, index) => (
+          <h3 className="text-xl font-semibold flex items-center">
+            <MonitorWeightIcon sx={{ mr: 1 }} />
+            Peso:
+          </h3>
+          <p className="text-gray-700">{details.peso} Kg</p>
+        </div>
+        <div className="mb-8">
+          <h3 className="text-xl font-semibold flex items-center">
+            <HealingIcon sx={{ mr: 1 }} />
+            Síntomas presentados
+          </h3>
+          <ul className="list-disc list-inside text-gray-700 ml-7">
+            {details.sintomas.map((sintoma, index) => (
               <li key={index}>{sintoma.trim()}</li>
             ))}
           </ul>
         </div>
         <div className="mb-8">
-          <h3 className="text-xl font-semibold">Vacunas suministradas</h3>
-          <ul className="list-disc list-inside text-gray-700">
-            {selectedItem.vacunas.split(",").map((vacuna, index) => (
+          <h3 className="text-xl font-semibold flex items-center">
+            <VaccinesIcon sx={{ mr: 1 }} />
+            Vacunas suministradas
+          </h3>
+          <ul className="list-disc list-inside text-gray-700 ml-7">
+            {details.vacunas.map((vacuna, index) => (
               <li key={index}>{vacuna.trim()}</li>
             ))}
           </ul>
         </div>
         <div className="mb-8">
-          <h3 className="text-xl font-semibold">Tratamientos recetados</h3>
-          <ul className="list-disc list-inside text-gray-700">
-            {selectedItem.tratamientos.split(",").map((tratamiento, index) => (
+          <h3 className="text-xl font-semibold flex items-center">
+            <TreatmentIcon sx={{ mr: 1 }} />
+            Tratamientos recetados
+          </h3>
+          <ul className="list-disc list-inside text-gray-700 ml-7">
+            {details.tratamientos.map((tratamiento, index) => (
               <li key={index}>{tratamiento.trim()}</li>
             ))}
           </ul>
@@ -63,3 +98,17 @@ const DetailedRecordInfo = ({ open, handleClose, selectedItem }) => {
 };
 
 export default DetailedRecordInfo;
+
+DetailedRecordInfo.propTypes = {
+  open: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+  selectedItem: PropTypes.shape({
+    consulta_id: PropTypes.string.isRequired,
+    nombre_mascota: PropTypes.string.isRequired,
+    fecha: PropTypes.string.isRequired,
+    peso: PropTypes.string.isRequired,
+    sintomas: PropTypes.string.isRequired,
+    vacunas: PropTypes.string.isRequired,
+    tratamientos: PropTypes.string.isRequired,
+  }).isRequired,
+};
