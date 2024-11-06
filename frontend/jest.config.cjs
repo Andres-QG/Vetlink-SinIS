886a314654a52e76387a3ddeb516059b3d32e7cb
andrey1099-ucr <lester.pereira@ucr.ac.cr>
2024-11-05 22:49:35 -0600

    FEAT: Pruebas de consultar mis mascotas listas

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,8 +1,9 @@
-export default {
+module.exports = {
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/src/__mocks__/fileMock.js",
   },
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
